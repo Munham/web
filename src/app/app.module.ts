705f6732c3cbf05a25a3d6fd8a783f8f5e35af9f
Munham <munhammazrui@gmail.com>
2023-01-31 19:41:09 -0800

    firt commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './mypages/signup/signup.component';
+import { LoginComponent } from './mypages/login/login.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent
+   
+  
   ],
   imports: [
     BrowserModule,
